4c895f4f6aaf386ca9ead4d51d6edd12da7ebb49
Keenan Jones <keenanjones210@gmail.com>
2020-12-18 09:47:49 -0600

    KEY

--- src/components/currentlyReading.js
+++ src/components/currentlyReading.js
@@ -5,31 +5,40 @@ class currentlyReading extends Component {
   super()
   this.state = {
   name: "",
-  books:[]
+  books:[],
+  bookSearch: ""
   }
  }
 
- componentDidMount(){
-  // this.setState({
-  //  name: this.props.currList.name,
-  //  books: this.props.currList.reading_list_books
-  // })
- }
 
  renderList = () => {
   if(this.props.currList){
    let bookList = this.props.currList.reading_list_books.map(ele => ele.book)
 
-  return bookList.map(book => <p>{book.title}</p>)
+  return bookList.map(book => <p>Are you still reading {book.title}?</p>)
   }
+ }
+
+ handleSearch = (e) => {
+  this.setState({
+   bookSearch: e.target.value
+  })
  }
 
  render() {
   return (
    <div className="col-3" id="homeSecondaryColumn">
     <h5>Currently Reading</h5>
+
+    <div className="my-3">What are you reading?
+     <form action="">
+      <input type="text" onChange={(e) => this.handleSearch(e)} value={this.state.bookSearch}/>
+      <input type="submit" name="" id="" />
+     </form>
+    </div>
     {/* {this.props.currList !== 'undefined' ? this.renderList() : null} */}
     {this.renderList()}
+
    </div>
   )
  }
